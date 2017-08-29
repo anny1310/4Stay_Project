78d7fa5c3b559ae1127854c249fda38f10ac09ef
marinatsallagova <tsallagovams@gmail.com>
2017-08-29 17:17:44 -0400

    Updated one line

--- src/test/java/com/fourstay/runners/CukesRunner.java
+++ src/test/java/com/fourstay/runners/CukesRunner.java
@@ -6,12 +6,10 @@ import cucumber.api.CucumberOptions;
 import cucumber.api.junit.Cucumber;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(
-		plugin={"html:target/cucumber-reports", "json:target/cucmber.json"},
-		features ="./src/test/resources/features",
-		glue = "com.fourstay.step_definitions/",
-		tags= "@regression"
-              // dryRun=false
+@CucumberOptions(plugin = { "html:target/cucumber-reports",
+		"json:target/cucmber.json" }, features = "./src/test/resources/features", glue = "com.fourstay.step_definitions/", tags = "@regression"
+// dryRun=false
+// jfskdghdslgdfjsdl
 )
 
 public class CukesRunner {
